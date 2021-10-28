b2f73f60dc24094a8b847749c6712ea81bd2bf7f
Super Zombi <rost201541@gmail.com>
2021-10-28 19:44:21 +0300

    Update lang_root.js

--- root_/scripts/lang_root.js
+++ root_/scripts/lang_root.js
@@ -1,12 +1,10 @@
-(async function(){
-	if (location.hash){
-		language = location.hash.split("#")[1]
-	}
-	else{
-		language = window.navigator.language.substr(0, 2)
-		location.hash = language
-	}
-	var l = document.createElement("script")
-	l.setAttribute("src", `root_/Langs/${language.toUpperCase()}.json`);
-	await document.head.appendChild(l)	
-})()
+if (location.hash){
+	language = location.hash.split("#")[1]
+}
+else{
+	language = window.navigator.language.substr(0, 2)
+	location.hash = language
+}
+var l = document.createElement("script")
+l.setAttribute("src", `root_/Langs/${language.toUpperCase()}.json`);
+document.head.appendChild(l)
