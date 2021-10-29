3d675e471cee3037254e27f20621c2d989e0a1fb
Super Zombi <rost201541@gmail.com>
2021-10-29 13:46:40 +0300

    Update lang.js

--- root_/scripts/lang.js
+++ root_/scripts/lang.js
@@ -14,7 +14,7 @@ document.head.appendChild(l);
 		setTimeout(function(){
 			if (typeof LANG === 'undefined'){
 				var l = document.createElement("script")
-				l.setAttribute("src", `root_/Langs/EN.json`);
+				l.setAttribute("src", `../../root_/Langs/EN.json`);
 				document.head.appendChild(l);
 				var th = location.hash.split("#")[2]
 				if (typeof th === 'undefined'){
