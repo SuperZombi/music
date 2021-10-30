50b812673d22ada0a92b3aec5cb5ed289c67c2d1
Super Zombi <rost201541@gmail.com>
2021-10-30 14:45:28 +0300

    Add files via upload

---
+++ root_/scripts/requirements.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+	(function load_page(){
+	if (typeof header !== 'undefined' && typeof body !== 'undefined' && typeof footer !== 'undefined'){
+		document.title = `Zombi Music - ${LANG.requirements_title}`
+		document.body.innerHTML += header
+		document.body.innerHTML += body
+		document.body.innerHTML += footer
+
+		setTimeout(function(){document.body.style.transition = "1s"}, 500)
+	}
+	else{
+		setTimeout(function(){load_page()}, 500)
+	}
+	})()
+}
+
+function try_dark(e){
+	if (darkThemeMq){
+		e.src = e.src.split('.').slice(0, -1).join('.') + "_dark.svg"
+	}
+	else{
+		e.src = e.src.split('.').slice(0, -1).join('.').split("_dark")[0] + ".svg"
+	}
+}
