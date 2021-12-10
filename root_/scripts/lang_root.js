bf791c996038c5089d7821a4057e57a2481d64d8
Super Zombi <rost201541@gmail.com>
2021-12-10 20:43:34 +0200

    Update lang_root.js

--- root_/scripts/lang_root.js
+++ root_/scripts/lang_root.js
@@ -5,29 +5,31 @@ else{
 	language = window.navigator.language.substr(0, 2)
 	location.hash = language
 }
-var l = document.createElement("script")
-l.setAttribute("src", `root_/Langs/${language.toUpperCase()}.json`);
-document.head.appendChild(l);
 
-(function (){
-	if (typeof LANG === 'undefined'){
-		setTimeout(function(){
-			if (typeof LANG === 'undefined'){
-				setTimeout(function (){
-					if (typeof LANG === 'undefined'){
-						var l = document.createElement("script")
-						l.setAttribute("src", `root_/Langs/EN.json`);
-						document.head.appendChild(l);
-						var th = location.hash.split("#")[2]
-						if (typeof th === 'undefined'){
-							location.hash = "en"
-						}
-						else{
-							location.hash = `#en#${th}`
-						}
-					}
-				}, 500)
+(function executeIfFileExist() {
+	var src = `root_/Langs/${language.toUpperCase()}.json`;
+    var xhr = new XMLHttpRequest()
+    xhr.open('HEAD', src, true)
+    xhr.onreadystatechange = function() {
+        if (this.readyState === this.DONE) {
+        	if (xhr.status==200){
+				var l = document.createElement("script")
+				l.setAttribute("src", `../root_/Langs/${language.toUpperCase()}.json`);
+				document.head.appendChild(l);
 			}
-		}, 500)
-	}
+			else{
+				var l = document.createElement("script")
+				l.setAttribute("src", `../../root_/Langs/EN.json`);
+				document.head.appendChild(l);
+				var th = location.hash.split("#")[2]
+				if (typeof th === 'undefined'){
+					location.hash = "en"
+				}
+				else{
+					location.hash = `#en#${th}`
+				}
+			}
+        }
+    }
+    xhr.send();
 })()
