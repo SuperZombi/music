bf1065f1a80b8da8250f7bcdec946fbc11255010
Super Zombi <rost201541@gmail.com>
2021-12-10 20:46:14 +0200

    Update lang_root.js

--- root_/scripts/lang_root.js
+++ root_/scripts/lang_root.js
@@ -8,18 +8,18 @@ else{
 
 (function executeIfFileExist() {
 	var src = `root_/Langs/${language.toUpperCase()}.json`;
-    var xhr = new XMLHttpRequest()
-    xhr.open('HEAD', src, true)
-    xhr.onreadystatechange = function() {
-        if (this.readyState === this.DONE) {
-        	if (xhr.status==200){
+	var xhr = new XMLHttpRequest()
+	xhr.open('HEAD', src, true)
+	xhr.onreadystatechange = function() {
+		if (this.readyState === this.DONE) {
+			if (xhr.status==200){
 				var l = document.createElement("script")
-				l.setAttribute("src", `../root_/Langs/${language.toUpperCase()}.json`);
+				l.setAttribute("src", `root_/Langs/${language.toUpperCase()}.json`);
 				document.head.appendChild(l);
 			}
 			else{
 				var l = document.createElement("script")
-				l.setAttribute("src", `../../root_/Langs/EN.json`);
+				l.setAttribute("src", `root_/Langs/EN.json`);
 				document.head.appendChild(l);
 				var th = location.hash.split("#")[2]
 				if (typeof th === 'undefined'){
@@ -29,7 +29,7 @@ else{
 					location.hash = `#en#${th}`
 				}
 			}
-        }
-    }
-    xhr.send();
+		}
+	}
+	xhr.send();
 })()
