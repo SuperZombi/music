70834f34cce7124783f65a03c46350746d7b937a
Super Zombi <rost201541@gmail.com>
2022-02-10 01:52:06 +0200

    Add files via upload

---
+++ root_/scripts/parse_params.js
@@ -0,0 +1,46 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+if (params.theme){
+	if (params.theme == "dark"){
+		darkThemeMq = true
+		var l = document.createElement("link")
+		l.rel = "stylesheet"
+		l.setAttribute("href", "../../root_/styles/dark.css");
+		l.setAttribute("id", "dark_file");
+		document.head.appendChild(l)
+	}
+	else{
+		darkThemeMq = false
+	}
+}
+else{
+	darkThemeMq = false
+}
+
+if (params.lang){
+	language = params.lang
+}
+else{
+	language = window.navigator.language.substr(0, 2)
+}
+(function executeIfFileExist() {
+	var src = `../../root_/Langs/${language.toUpperCase()}.json`;
+	var xhr = new XMLHttpRequest()
+	xhr.open('HEAD', src, true)
+	xhr.onreadystatechange = function() {
+		if (this.readyState === this.DONE) {
+			if (xhr.status==200){
+				var l = document.createElement("script")
+				l.setAttribute("src", `../../root_/Langs/${language.toUpperCase()}.json`);
+				document.head.appendChild(l);
+			}
+			else{
+				var l = document.createElement("script")
+				l.setAttribute("src", `../../root_/Langs/EN.json`);
+				document.head.appendChild(l);
+			}
+		}
+	}
+	xhr.send();
+})()
