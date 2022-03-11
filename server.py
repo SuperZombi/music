c46fa9b0d5b8a6e6edbac1f86e4053eddbfe46ec
Super Zombi <rost201541@gmail.com>
2022-03-11 19:06:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -6,14 +6,45 @@ from flask import Flask, request, jsonify, send_from_directory, abort, redirect
 from flask_cors import CORS
 import json
 import re
+from enum import Enum
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+class Errors(Enum):
+	incorrect_name_or_password = {
+		'en': "The username or password you entered is incorrect!",
+		'ru': "Неверное имя пользователя или пароль!"
+	}
+	user_dont_exist = {
+		'en': "This user does not exist!",
+		'ru': "Такого пользователя не существует!"
+	}
+	name_already_taken = {
+		'en': "This nickname is already taken!",
+		'ru': "Этот ник уже занят!"
+	}
+	track_already_exists = {
+		'en': "Track already exists!",
+		'ru': "Трек уже существует!"
+	}
+	forbidden_character = {
+		'en': "Forbidden character in nickname!",
+		'ru': "Запрещённый символ в нике!"
+	}
+	creating_folder_error = {
+		'en': "Error creating folder on server!",
+		'ru': "Ошибка создания папки на сервере!"
+	}
+	invalid_parameters = {
+		'en': "Invalid parameters!",
+		'ru': "Неверные параметры!"
+	}
+
+@app.route("/status")
 def status():
 	return jsonify({'online': True, 'time': time.time()})
 
-@app.route('/data/<path:filepath>')
+@app.route('/<path:filepath>')
 def data(filepath):
 	p = os.path.join("data", filepath)
 	if os.path.exists(p):
@@ -27,7 +58,22 @@ def data(filepath):
 		return send_from_directory('data', filepath + '.html')
 	abort(404)
 
-@app.route('/get_tracks')
+@app.route("/api/decode_error", methods=["POST"])
+def get_error_value():
+	if 'lang' in request.json.keys():
+		lang = request.json['lang'].lower()
+
+	try:
+		temp = Errors[request.json['code']].value
+		if isinstance(temp, dict):
+			if lang in temp.keys():
+				return {'successfully': True, 'value': temp[lang]}
+			return {'successfully': True, 'value': temp['en']} # default
+		return temp
+	except:
+		return {'successfully': False}
+
+@app.route('/api/get_tracks')
 def get_tracks():
 	unswer = []
 	for user, array in tracks.items():
@@ -164,38 +210,40 @@ def artist_index(name, image="../root_/images/people.svg"):
 	</head><body></body></html>'''
 
 
-@app.route("/user_exists", methods=["POST"])
+@app.route("/api/user_exists", methods=["POST"])
 def user_exists_post():
 	if request.json['name'] in users.keys():
 		return jsonify({'exist': True})
 	return jsonify({'exist': False})
 
-@app.route("/name_available", methods=["POST"])
+@app.route("/api/name_available", methods=["POST"])
 def name_available():
+	if '"' in request.json['name']:
+		return jsonify({'available': False, 'reason': Errors.forbidden_character.name})
 	if request.json['name'] in users.keys():
-		return jsonify({'available': False})
+		return jsonify({'available': False, 'reason': Errors.name_already_taken.name})
 	user_folder = os.path.join("data", request.json['name'].lower().replace(" ", "-"))
 	if os.path.exists(user_folder):
-		return jsonify({'available': False})
+		return jsonify({'available': False, 'reason': Errors.creating_folder_error.name})
 	return jsonify({'available': True})
 
-@app.route("/login", methods=["POST"])
+@app.route("/api/login", methods=["POST"])
 def login():
 	if request.json['name'] in users.keys():
 		if users[request.json['name']]['password'] == request.json['password']:
 			return jsonify({'successfully': True})
 		else:
-			return jsonify({'successfully': False, 'reason':'Не верное имя пользователя или пароль!'})
+			return jsonify({'successfully': False, 'reason': Errors.incorrect_name_or_password.name})
 		
-	return jsonify({'successfully': False, 'reason':'Такого пользователя не существует!'})
+	return jsonify({'successfully': False, 'reason': Errors.user_dont_exist.name})
 
-@app.route("/register", methods=["POST"])
+@app.route("/api/register", methods=["POST"])
 def register():
 	if request.json['name'] in users.keys():
-		return jsonify({'successfully': False, 'reason':'Этот ник уже занят!'})
+		return jsonify({'successfully': False, 'reason': Errors.name_already_taken.name})
 	
 	if '"' in request.json['name']:
-		return jsonify({'successfully': False, 'reason':'Запрещённый символ в нике!'})
+		return jsonify({'successfully': False, 'reason': Errors.forbidden_character.name})
 
 	try:
 		user_folder = os.path.join("data", request.json['name'].lower().replace(" ", "-"))
@@ -206,15 +254,15 @@ def register():
 			with open(os.path.join(user_folder, 'index.html'), 'w', encoding='utf8') as file:
 				file.write(artist_index(request.json['name']))
 		else:
-			return jsonify({'successfully': False, 'reason':'Ошибка создании папки пользователя на сервере! Папка уже существует!'})
+			return jsonify({'successfully': False, 'reason': Errors.creating_folder_error.name})
 	except:
-		return jsonify({'successfully': False, 'reason':'Ошибка создании папки пользователя на сервере!'})
+		return jsonify({'successfully': False, 'reason': Errors.creating_folder_error.name})
 
 	try:
 		register_user(request.json)
 	except:
 		shutil.rmtree(user_folder)
-		return jsonify({'successfully': False, 'reason':'Неверные параметры!'})
+		return jsonify({'successfully': False, 'reason': Errors.invalid_parameters.name})
 
 	save_users()
 	return jsonify({'successfully': True})
@@ -250,7 +298,7 @@ def fast_login(user, password):
 			return True
 	return False
 
-@app.route('/uploader', methods=['POST'])
+@app.route('/api/uploader', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
 		if fast_login(request.form['artist'], request.form['password']):
@@ -259,7 +307,7 @@ def upload_file():
 
 			if os.path.exists(user_folder):
 				if track_exists(request.form['artist'], request.form['track_name']):
-					return jsonify({'successfully': False, 'reason':'Трек уже существует!'})
+					return jsonify({'successfully': False, 'reason': Errors.track_already_exists.name})
 
 				if not os.path.exists(track_folder):
 					os.makedirs(track_folder)
@@ -289,12 +337,12 @@ def upload_file():
 					except Exception as e:
 						print(e)
 						shutil.rmtree(track_folder)
-						return jsonify({'successfully': False, 'reason':'Неверные параметры!'})
+						return jsonify({'successfully': False, 'reason': Errors.invalid_parameters.name})
 			
-			return jsonify({'successfully': False, 'reason':'Ошибка создании папки трека на сервере!'})
+			return jsonify({'successfully': False, 'reason': Errors.creating_folder_error.name})
 
 		else:
-			return jsonify({'successfully': False, 'reason':'Не верное имя пользователя или пароль!'})
+			return jsonify({'successfully': False, 'reason': Errors.incorrect_name_or_password.name})
 
 		
 
